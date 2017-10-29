61b1093d688b98ae946df4be0714aa2d49744323
libo <libonero@didichuxing.com>
2017-10-29 20:35:03 +0800

    update

--- backend/app/Http/Controllers/V1/UserController.php
+++ backend/app/Http/Controllers/V1/UserController.php
@@ -40,15 +40,27 @@ class UserController extends Controller
     public function comments()
     {
         //这种筛选只能取得主模型的结果，关联模型的数据不能取得
-        $users = UserModel::whereHas('comments', function ($query){
+        $users = UserModel::whereHas('comments', function ($query) {
             $query->where('title', 'like', '%哈哈%');
-//        })->get();
-        });
+        })->get();
         echo "<pre>";
-        echo "至少一条评论标题带哈哈的人:";
+        echo "=====至少一条评论标题带哈哈的人=====";
         echo "<br>";
         foreach($users as $user){
-            echo $user['name'];
+            echo '评论人：'.$user->name;
+            echo "<br>";
+
+            /*var_dump($user->comments);
+            echo "<br>";
+            echo "--------------------------------";
+            var_dump($user->comments());*/
+
+            echo '所有评论：';
+            echo "<br>";
+            foreach ($user->comments as $comment){
+                echo $comment->title;
+                echo "<br>";
+            }
             echo "<br>";
         }
         echo "</pre>";
